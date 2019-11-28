cd7ee4cc722f43a5f76ad358bdaca0d5f236d5da
ruthbrennankk <ruthbrennankk@gmail.com>
2019-11-28 10:29:42 +0000

    d3js barchart works with data from github api

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -13,6 +13,8 @@ class App extends Component {
     this.state = {
       gitun: 'No username',
       info: '',
+      info2: null,
+      ready: false,
       formData: {
         username: '',
     }
@@ -23,11 +25,13 @@ class App extends Component {
 
 handleUserFormSubmit(event) {
     event.preventDefault();
-    axios.get('https://api.github.com/users/'+this.state.formData.username)
+    axios.get('https://api.github.com/users/'+this.state.formData.username+'/repos')
     .then(response => this.setState({
-      gitun: response.data.login,
-      info : JSON.stringify(response.data)
+      //gitun: response.data.login,
+      info : JSON.stringify(response.data,undefined, 2),
+      info2 : response.data
     })).catch((err) => { console.log(err); });
+    this.state.ready = true;
   };
 
 handleFormChange(event) {
@@ -35,6 +39,65 @@ handleFormChange(event) {
     obj[event.target.name] = event.target.value;
     this.setState(obj);
   };
+
+drawBarChart = () => {
+  if (this.state.ready) {
+    return (
+      <div>
+        <p>Size</p>
+        <BarChart data={this.getSizeArray()} size={[200,200]} />
+        <p>Stars</p>
+        <BarChart data={this.getStarsArray()} size={[200,200]} />
+      </div>
+    )
+  }
+};
+
+//Forks of repos
+getForksArray = () => {
+  console.log(this.state.info2.length);
+  var arr = [];
+  for (var i=0; i<this.state.info2.length;i++) {
+      arr[i] = this.state.info2[i].forks_count;
+  }
+  return arr;
+};
+
+//Size of repos
+getSizeArray = () => {
+  console.log(this.state.info2.length);
+  var arr = [];
+  for (var i=0; i<this.state.info2.length;i++) {
+      arr[i] = this.state.info2[i].size;
+  }
+  return arr;
+};
+
+//times the repo was starred
+getStarsArray = () => {
+  console.log(this.state.info2.length);
+  var arr = [];
+  for (var i=0; i<this.state.info2.length;i++) {
+      arr[i] = this.state.info2[i].stargazers_count;
+  }
+  return arr;
+};
+
+//current issues with the repo
+getIssuesArray = () => {
+  console.log(this.state.info2.length);
+  var arr = [];
+  for (var i=0; i<this.state.info2.length;i++) {
+      arr[i] = this.state.info2[i].open_issues_count;
+      console.log(arr[i]);
+  }
+  return arr;
+};
+
+
+//contributors
+//  "https://api.github.com/repos/ruthbrennankk/Software-Engineering-Essay/contributors"
+
 
 render() {
     return (
@@ -51,19 +114,17 @@ render() {
           handleFormChange={this.handleFormChange}
         />
         <p><b>Username:</b></p>
-        <p>{this.state.gitun}</p>
+        
         <b>Information:</b>
-        <pre>{this.state.info}</pre>
+          <pre>{this.state.info}</pre>
         <div className='App'>
-          <div className='App-header'>
-            <h2>d3ia dashboard</h2>
-          </div>
-          <div>
-            <BarChart data={[5,10,1,3]} size={[300,300]} />
-          </div>
+          {this.drawBarChart()}
         </div>
+        
 </div>
     );
   }
 }
 export default App;
+
+////<p>{this.state.gitun}</p>
